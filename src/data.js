56b5cd18e9de782fa5fe46035fd226fcf453290d
Vishal <p.vishal140220@gmail.com>
2022-01-25 21:43:59 +0530

    project link changes

--- src/data.js
+++ src/data.js
@@ -3,12 +3,12 @@ export const products = [
     id: 1,
     img: "https://images.ctfassets.net/lzny33ho1g45/O6Ns6DttUzJym7rhGiD36/68646ae82f982ea77bdea3f24eca79bf/best-todo-list-apps-00-hero.jpg?w=1520&fm=jpg&q=30&fit=thumb&h=760",
     desc:"To-do App",
-    link: "https://github.com/vishal140220/Todo-App--react",
+    link: "https://saveyourtime.netlify.app/",
   },
   {
     id: 2,
     img: "https://www.patriotsoftware.com/wp-content/uploads/2019/10/what-is-expense-account.jpg",
     desc:"Expense Tracker",
-    link: "https://github.com/vishal140220/ExpenseTracker",
+    link: "https://control-expenses.netlify.app/",
   }
 ];
